41f824e87c2371a3bb688c7cce3785adf5139616
Janu Foley <160372979+JanuF@users.noreply.github.com>
2025-08-02 14:30:43 +1200

    Working camera

--- camera.js
+++ camera.js
@@ -1,33 +1,33 @@
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then((stream) => {
-        const video = document.getElementById("camera");
-        video.srcObject = stream;
-    })
-    .catch((error) => {
-    console.error("Error with camera", error);
-    })
+document.addEventListener("DOMContentLoaded", () => {
+    navigator.mediaDevices.getUserMedia({video: true})
+        .then((stream) => {
+            const video = document.getElementById("camera");
+            video.srcObject = stream;
+        })
+        .catch((error) => {
+            console.error("Error with camera", error);
+        })
 
-const captureBtn = document.getElementById('capture-btn');
-const canvas = document.getElementById('photo');
-const context = canvas.getContext('2d');
-const dowloadLink = document.createElement('a');
-dowloadLink.innerText = 'Dowload Photo';
-document.body.appendChild(dowloadLink);
+    const captureBtn = document.getElementById('capture-btn');
+    const canvas = document.getElementById('photo');
+    const context = canvas.getContext('2d');
 
-captureBtn.addEventListener('click', () => {
-    const video = document.getElementById('camera');
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    captureBtn.addEventListener('click', () => {
+        const video = document.getElementById('camera');
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
 
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    const dataURL = canvas.toDataURL('image/png');
+        const dataURL = canvas.toDataURL('image/png');
 
-    dowloadLink.href = dataURL;
-    dowloadLink.download = 'captured-photo.png';
-    dowloadLink.style.display = 'block';
-    dowloadLink.click();
-});
+        const dowloadLink = document.createElement('a');
+        dowloadLink.href = dataURL;
+        dowloadLink.download = 'captured-photo.png';
+        dowloadLink.style.display = 'block';
+        dowloadLink.click();
+    });
+})
 
 
 
