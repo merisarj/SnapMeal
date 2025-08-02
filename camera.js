7222e79f6f77ffb75b97e66f9cf3fc2bd4a82629
Janu Foley <160372979+JanuF@users.noreply.github.com>
2025-08-02 12:54:47 +1200

    Camera v2

--- camera.js
+++ camera.js
@@ -7,3 +7,27 @@ navigator.mediaDevices.getUserMedia({ video: true })
     console.error("Error with camera", error);
     })
 
+const captureBtn = document.getElementById('capture-btn');
+const canvas = document.getElementById('photo');
+const context = canvas.getContext('2d');
+const dowloadLink = document.createElement('a');
+dowloadLink.innerText = 'Dowload Photo';
+document.body.appendChild(dowloadLink);
+
+captureBtn.addEventListener('click', () => {
+    const video = document.getElementById('camera');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const dataURL = canvas.toDataURL('image/png');
+
+    dowloadLink.href = dataURL;
+    dowloadLink.download = 'captured-photo.png';
+    dowloadLink.style.display = 'block';
+});
+
+
+
+
